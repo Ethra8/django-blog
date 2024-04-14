0e55f0e28eb80aa7edc632070fd7c3e3188b9e28
Edna TMunill <ethra822@gmail.com>
2024-04-14 22:56:55 +0000

    debug true

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-ethra8-djangoblog-6isvylf4sa8.ws-eu110.gitpod.io', '.herokuapp.com']
 
