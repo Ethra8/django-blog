405a43cfcdfe3daaf6e1e81f311ff90cce6b857d
Edna TMunill <ethra822@gmail.com>
2024-04-21 21:10:10 +0000

    add style to sing up template

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,9 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ethra8-djangoblog-f7hms4490vm.ws-eu110.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ethra8-djangoblog-aalmqupqc1x.ws-eu110.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
