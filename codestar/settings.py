764274ea0892cd09be1620e9b4e53f780a5a3719
Edna TMunill <ethra822@gmail.com>
2024-04-22 14:01:47 +0000

    display approved comments

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ethra8-djangoblog-aalmqupqc1x.ws-eu110.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ethra8-djangoblog-271eg9pw2ur.ws-eu110.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
